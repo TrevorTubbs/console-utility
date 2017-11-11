a2131811e5719405d9a9fc1492ac7fc4c28a9179
TrevorTubbs <eighteighteightyeight@gmail.com>
2017-11-11 16:14:26 -0600

    Added basic parameter parsing.

---
+++ Source/ConsoleUtility.Test/Input/ArgumentReaderTests.cs
@@ -0,0 +1,57 @@
+﻿using ConsoleUtility.Input;
+using NUnit.Framework;
+
+namespace ConsoleUtility.Test.Input {
+    [TestFixture]
+    public class ArgumentReaderTests {
+        [SetUp]
+        public void SetUp() {
+            Reader = new ArgumentReader();
+        }
+
+        [TearDown]
+        public void TearDown() {
+            Reader = null;
+        }
+
+        private ArgumentReader Reader { get; set; }
+
+        [Test]
+        public void SetArgumentsAcceptsNull() {
+            Reader.SetArguments(null);
+        }
+
+        [Test]
+        public void SetArgumentsAcceptsEmptyArray() {
+            Reader.SetArguments(new string[0]);
+        }
+
+        [Test]
+        public void SetArgumentsAcceptsArrayWithNullElement() {
+            Reader.SetArguments(new[] { "foo", null, "bar" });
+        }
+
+        [TestCaseSource("ArgumentsTestCaseSource")]
+        public void GetValue(string[] arguments, string key, string expected) {
+            Reader.SetArguments(arguments);
+
+            string actual = Reader.GetValue(key);
+
+            Assert.AreEqual(expected, actual, $"GetValue(\"{key}\")");
+        }
+
+        public static object[] ArgumentsTestCaseSource {
+            get {
+                var argumentList1 = new[] { "seed=87", "key=lkljsdf", "autostart", "name=Jake", "hint=42=21+21" };
+                return new object[] {
+                    new object[] { argumentList1, "seed", "87" },
+                    new object[] { argumentList1, "key", "lkljsdf" },
+                    new object[] { argumentList1, "autostart", "true" },
+                    new object[] { argumentList1, "autostop", "false" },
+                    new object[] { argumentList1, "name", "Jake" },
+                    new object[] { argumentList1, "hint", "42=21+21" }
+                };
+            }
+        }
+    }
+}
---
+++ Source/ConsoleUtility/Input/ArgumentReader.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleUtility.Input {
+    public class ArgumentReader {
+        public void SetArguments(string[] arguments) {
+            if (arguments == null) {
+                Values = null;
+                return;
+            }
+
+            Values = new Dictionary<string, string>();
+            foreach (string argument in arguments) {
+                Process(argument);
+            }
+        }
+
+        private void Process(string argument) {
+            if (string.IsNullOrWhiteSpace(argument))
+                return;
+
+            var parts = argument.Split('=');
+            if (parts.Length == 1)
+                Values.Add(parts[0], "true");
+            if (parts.Length == 2)
+                Values.Add(parts[0], parts[1]);
+            if (parts.Length > 2)
+                Values.Add(parts[0], string.Join("=", parts, 1, parts.Length - 1));
+        }
+
+
+        public string GetValue(string key) {
+            if (Values == null)
+                return null;
+            if (Values.ContainsKey(key))
+                return Values[key];
+            return "false";
+        }
+
+        private Dictionary<string, string> Values { get; set; }
+    }
+}
